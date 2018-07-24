8d55b730e988a7027733cf1ff77a79c0e98bc641
lishixuan001 <lishixuan001@berkeley.edu>
2018-07-24 01:58:19 -0400

    section commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Admin Superuser:
 Username: cs160-proj5
-Password: cs160-project5
+Password: cs160-proj5123
--- life/views.py
+++ life/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render
 from django.core import serializers
 from django.utils import timezone
+from django.http import HttpResponseRedirect
 from life.models import *
 
-question_text = ""
-question_description = ""
-userList = User.objects.all()
-user_self = None
-if len(userList) > 0:
-  user_self = userList[0]
+# question_text = ""
+# question_description = ""
+# userList = User.objects.all()
+# user_self = None
+# if len(userList) > 0:
+#     user_self = userList[0]
+
+# Default User -> Anonymous (id=5)
+currentUser = User.objects.get(id=5)
 
 def index(request):
     return render(request, 'life/index.html')
   
 def forum(request):
-  if request.method == 'POST':
-    if user_self != None:
-      Question.objects.create(question=question_text, description=question_description,
-                              creation_date=timezone.now(), user = user_self, last_modified=timezone.now())
-  else:
-    pass
-  return render(request, 'life/forum.html')
+    question_list = Question.objects.all()
+    context = {
+        'question_list': question_list
+    }
+    return render(request, 'life/forum.html', context)
 
 def forum_create(request):
     if request.method == 'POST':
-        question_text = request.POST['question-text'].strip()
-        question_description = request.POST['question-description'].strip()
+        question_text = request.POST['question'].strip()
+        question_description = request.POST['description'].strip()
+        Question.objects.create(user=currentUser, question=question_text, description=question_description)
+        return HttpResponseRedirect('/life/forum/')
     else:
         pass
     return render(request, 'life/forum_create.html')
@@ -44,3 +48,42 @@ def case_create(request):
 
 def tracker(request):
 	return render(request, 'life/tracker.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# END
